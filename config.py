1cbf74fea4bac1f0e54f12797ffc19e0f0423686
Princepia <princepia.motox@gmail.com>
2026-08-23 18:07:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+from os import getenv
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "20807000"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","cde2366a7c61e23f4cb44618cbe6cf70")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8564398983:AAGxMpPkmLcgZsPnVzIQzCUIro5KNk76QBw")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@nxtgenx_bot)
+BOT_TEXT = "GAJJU BHAI KA EXTRACTOR"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "890749443"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003646612944"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003646612944")) 
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://princepia161_db_user:KZWhQUyEe59dF1jh@cluster0.uhoxe0h.mongodb.net/?appName=Cluster0")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003646612944"))
+# -----------------------------------------------
+join = '<a href="https://t.me/princepia">✳️ JOIN BACKUP</a>'
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "Princepia" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://josephscollege.ac.in/wp-content/uploads/2022/04/1.jpg")
+
+
+
+
+# # Bot configuration
+# API_ID = int(os.environ.get("API_ID", "20807000"))
+# API_HASH = os.environ.get("API_HASH", "cde2366a7c61e23f4cb44618cbe6cf70")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8564398983:AAGxMpPkmLcgZsPnVzIQzCUIro5KNk76QBw")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "@nxtgenx_bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "890749443"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003646612944"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003646612944"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
+
